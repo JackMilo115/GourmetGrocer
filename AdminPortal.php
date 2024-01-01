b1b1529ac7e69bb225a51fc00c2e00334d752a9c
Jack Milo <mil21001248@btc.ac.uk>
2024-01-01 12:49:08 +0000

    Added Admin Portal

---
+++ AdminPortal.php
@@ -0,0 +1,5 @@
+<?php $title = 'Admin Page'; require __DIR__ . "/inc/header.php"; ?>
+     
+<?php require __DIR__ . "/components/AdminPortal.php"; ?>
+
+<?php require __DIR__ . "/inc/footer.php"; ?>
