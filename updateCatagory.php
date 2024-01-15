fbee095c2942f425edcf1ad36bf6c6c63edcc14c
Jack Milo <mil21001248@btc.ac.uk>
2024-01-15 13:27:44 +0000

    added update catagory and update supplier forms

---
+++ updateCatagory.php
@@ -0,0 +1,17 @@
+<?php
+    $title = 'Admin Update Catagory Page';
+    require __DIR__ . "/inc/header.php";
+
+
+    if (isset($_SESSION['user']))
+    {
+        if ($controllers->members()->get_role_by_id($_SESSION['user']['ID'])['role_id'] !== 2)
+        {
+            redirect('login', ["error" => "You need to be an admin to view this page"]);
+        }
+    }
+
+    require __DIR__ . "/components/update-catagory-form.php";
+
+    require __DIR__ . "/inc/footer.php";
+?>
